9772f6b69a6a15dc82254c164a00a8f1e785fc75
brommz <mydew1992@gmail.com>
2022-02-17 10:11:22 +0100

    dict-by-ref sample

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace dict_by_ref
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new System.Collections.Generic.Dictionary<string, TestingStruct>()
+            {
+                { "k1", new TestingStruct(1) },
+                { "k10", new TestingStruct(10) }
+            };
+
+            var value1 = dict["k1"];
+            value1.Number = 100;
+
+            System.Console.Out.WriteLine($"Normay way: {value1.Number == dict["k1"].Number}");
+
+            ref var value1Ref = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, "k1", out _);
+            value1Ref.Number = 200;
+
+            System.Console.Out.WriteLine($"With CollectionsMarshal: {value1Ref.Number == dict["k1"].Number}");
+            System.Console.Out.WriteLine($"Value: {dict["k1"].Number}");
+        }
+    }
+
+    public struct TestingStruct
+    {
+        public int Number;
+        public TestingStruct(int num)
+        {
+            Number = num;
+        }
+    }
+}
